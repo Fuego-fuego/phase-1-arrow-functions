36b29145d18c0f79f7691f91403d3b71978b301a
nathaniEL mujesia <nathanielmujesia@gmail.com>
2023-12-13 09:55:29 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+// Divide f(x) expression : divides 2 numbers 
+
+const divide = function(dividend = 2000, divisor = 100){
+
+    const  quotient = dividend / divisor; 
+
+    return quotient;
+}
+
+// Squaring f(x), defined using an arrow f(x)
+const square = number => number * number ;
+
+// Adding f(x), defined using an arrow f(x)
+
+const add = (num1, num2) => num1 + num2; 
